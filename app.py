1838b282d8c132cba099399b8c46737c3c33e7d4
Diego Orellana <benderrobot12@gmail.com>
2020-05-22 18:11:23 -0600

    Add firts templates to project create static and template folders

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/simple-map')
+def simple_map():
+    return render_template('simple_map.html')
