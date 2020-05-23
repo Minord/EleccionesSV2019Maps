8e27c430cce5f19467bd376af83d5edb88f6e2d3
Minord <benderrobot12@gmail.com>
2020-05-22 22:17:59 -0600

    Add milligram css style

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 app = Flask(__name__)
+
+#Comands to execute the app for windows
+#venv/Scripts/activate
+# $env:FLASK_ENV = "development"
+#set FLASK_APP= app.py
+#flask run
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 
 @app.route('/simple-map')
