03218bea78cd1e74ecb4ece10afb4745e34234ae
Minord <benderrobot12@gmail.com>
2020-05-27 20:39:20 -0600

    the data base credentials are store in a ignore db.conf  file

--- db.py
+++ db.py
@@ -1,5 +1,7 @@
 #include and add postgres sql to the project
 
+import configparser
+
 import psycopg2 # PostgreSql DataBase Adapter.
 
 import click
@@ -8,7 +10,21 @@ from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not  in g:
-        g.db = psycopg2.connect("dbname = elections2019SV user = postgres password = secret")
+        #get credentials
+        dbname = None
+        user = None
+        password = None
+        db_conf = get_db_conf()
+        if db_conf is not None:
+            dbname = db_conf['DEFAULT']['DB_NAME']
+            user = db_conf['DEFAULT']['DB_USER']
+            password = db_conf['DEFAULT']['DB_PASSWORD']
+        else:
+            dbname = input('database name: ')
+            user = input('user name: ')
+            password = input('password name: ')
+
+        g.db = psycopg2.connect("dbname = {} user = {} password = {}".format(dbname, user, password))
     return g.db
 
 def close_db(e=None):
@@ -19,3 +35,12 @@ def close_db(e=None):
 
 def init_app(app):
     app.teardown_appcontext(close_db)
+
+
+def get_db_conf():
+    config = configparser.ConfigParser()
+    config.read('db.conf')
+    if len(config) <= 0:
+        return None
+
+    return config
