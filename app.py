b847b2e9a999c085b865ad986c8e00f93c1df9b0
Diego Orellana <benderrobot12@gmail.com>
2020-06-27 22:10:18 -0600

    Add the departamentos nested queries and endpoints

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import os
 import json
 
+
 from flask import Flask, jsonify
 from flask import render_template, url_for
 from . import db
 
+#add snaql for handle the sql in separate files
 import psycopg2.extras
 from snaql.factory import Snaql
 
+#create the classes and instances for make use of the queries to DB
 root_location = os.path.abspath(os.path.dirname(__file__))
 snaql_factory = Snaql(root_location, 'queries')
 limits_queries = snaql_factory.load_queries('limits.sql')
+
 #creating the app
 app = Flask(__name__)
-
 db.init_app(app)
 
 #Comands to execute the app for windows
@@ -22,19 +25,29 @@ db.init_app(app)
 #set FLASK_APP= app.py
 #flask run
 
+
+
 #################### NAVEGABLE PAGES ###########################
 @app.route('/')
 def hello_world():
+    """root end point it render the index page of the app"""
     return render_template('index.html')
 
 
 @app.route('/simple-map')
 def simple_map():
+    """this end point show a basic version of the map in a page"""
     return render_template('simple_map.html')
 
 @app.route('/sandbox')
 def sandbox():
+    """this is a experimental end point for test experimental pages"""
     return render_template('sandbox.html')
+
+
+
+
+
 ##################### GEO DATA PROVIDERS ###################
 @app.route('/geoserver')
 def geo_directory():
@@ -42,6 +55,7 @@ def geo_directory():
 
 @app.route('/geoserver/departementos.geojson')
 def geo_departamentos():
+    """this return a geo json that has the el salvador departamentos"""
     db_conn = db.get_db()
     #queries
     cur = db_conn.cursor()
@@ -53,6 +67,7 @@ def geo_departamentos():
 
 @app.route('/geoserver/municipios.geojson')
 def geo_municipios():
+    """this is a GET that return a geojson that has all the country municipios"""
     db_conn = db.get_db()
     #queries
     cur = db_conn.cursor()
@@ -64,6 +79,7 @@ def geo_municipios():
 
 @app.route('/geoserver/cantones.geojson')
 def geo_cantones():
+    """this is a GET that return a geojson that has all the cantones in the country"""
     db_conn = db.get_db()
     #queries
     cur = db_conn.cursor()
@@ -73,20 +89,46 @@ def geo_cantones():
     cur.close()
     return str(result[0]).replace("'", '"')
 
-@app.route('/geoserver/minicipios-in-dep/<dep>.geojson')
-def geo_cantones():
+@app.route('/geoserver/municipios-dep-<cod_dep>.geojson')
+def geo_municipios_in_dep(cod_dep):
+    """this is a GET that return a geojson that has all the municipios in a dep"""
+    #in this case can be a good idea check if the dep_code exits.
+    arguments = {'cod_dep': int(cod_dep) }
+    #get the data base connection
     db_conn = db.get_db()
     #queries
     cur = db_conn.cursor()
-    cur.execute(limits_queries.cantones_geojson())
+    cur.execute(limits_queries.municipios_in_dep(**arguments))
+
+    #get results from queries
     result = cur.fetchone()
+    
+    #cerrar la base de datos
+    db_conn.commit()
+    cur.close()
+
+    return str(result[0]).replace("'", '"')
+
+@app.route('/geoserver/cantones-dep-<cod_dep>.geojson')
+def geo_cantones_in_dep(cod_dep):
+    """this is a GET that return a geojson that has all the cantones in a departamento"""
+    #maybe do dep_code validation
+    arguments = {'cod_dep' : cod_dep}
+    #get db connection
+    db_conn = db.get_db()
+    #queries
+    cur = db_conn.cursor()
+    cur.execute(limits_queries.cantones_in_dep(**arguments))
+    #get result from query
+    result = cur.fetchone()
+    #close the db
     db_conn.commit()
     cur.close()
     return str(result[0]).replace("'", '"')
-
 
 @app.route('/dbtest')
 def database_test():
+    """It this end point is for develop experimental features"""
     db_conn = db.get_db()
     #handle with data base
     cur = db_conn.cursor()
@@ -95,7 +137,3 @@ def database_test():
     db_conn.commit()
     cur.close()
     return (str(result))
-
-    #.encode('latin-1').decode("utf-8")
-#test end points delete this.
-
--- queries/limits.sql
+++ queries/limits.sql
@@ -23,7 +23,7 @@ SELECT row_to_json(result_s1) FROM
 	FROM (
         --  create a featues as json
 		SELECT 'Feature' as type, -- type prop
-		json_build_object ('cod_munic', cod_munic, 'nombre', nombre) as properties, -- properties prop
+		json_build_object ('cod_munic', cod_munic,'cod_dep', cod_dep, 'nombre', nombre) as properties, -- properties prop
 		cast(ST_AsGeoJson(ST_Simplify(pos_geom, 0)) as json) as geometry -- geometry in geojson format is a muti-polygon
 		FROM municipios
 	) AS result_f
@@ -39,7 +39,7 @@ SELECT row_to_json(result_s1) FROM
 	FROM (
         --  create a featues as json
 		SELECT 'Feature' as type, -- type prop
-		json_build_object ('cod_canton', cod_canton, 'nombre', nombre) as properties, -- properties prop
+		json_build_object ('cod_canton', cod_canton, 'cod_munic', cod_munic, 'nombre', nombre) as properties, -- properties prop
 		cast(ST_AsGeoJson(ST_Simplify(pos_geom, 0)) as json) as geometry -- geometry in geojson format is a muti-polygon
 		FROM cantones
 	) AS result_f
@@ -59,8 +59,8 @@ SELECT row_to_json(result_s1) FROM (
 		cast(ST_AsGeoJson(pos_geom) as json) as geometry
 		FROM municipios
 		WHERE cod_dep = {{ cod_dep|guards.integer }}
-	) AS result_f;
-) as result_s1;
+	) AS result_f
+) as result_s1
 
 {% endsql %}
 
@@ -68,22 +68,39 @@ SELECT row_to_json(result_s1) FROM (
 
 
 WITH valid_cods_munic_arr AS (
-	SELECT ARRAY (
-		SELECT cod_munic 
-		FROM municipios
-		WHERE cod_dep = {{ cod_dep|guards.integer }} 
-	)
+	SELECT cod_munic 
+	FROM municipios
+	WHERE cod_dep = {{ cod_dep | guards.integer }}
 )
 
 SELECT row_to_json(result_s1) FROM (
 	SELECT 'FeatureCollection' as type, array_agg(row_to_json(result_f)) as features
 	FROM (
 		SELECT 'Feature' as type,
-		json_build_object ('cod_munic', cod_munic, 'nombre', nombre) as properties,
+		json_build_object ('cod_canton', cod_canton,'cod_munic', cantones.cod_munic, 'nombre', nombre) as properties,
 		cast(ST_AsGeoJson(pos_geom) as json) as geometry
 		FROM cantones
-		WHERE cod_munic IN valid_cods_munic_arr
-	) AS result_f;
-) as result_s1;
+		INNER JOIN valid_cods_munic_arr ON cantones.cod_munic = valid_cods_munic_arr.cod_munic
+	) AS result_f
+) as result_s1
 
 {% endsql %}
+
+{% sql 'munic_resultado_in_dep', note = 'This query return a geojson'%}
+
+SELECT row_to_json(result_s1) FROM (
+	SELECT 'FeatureCollection' as type, array_agg(row_to_json(result_f)) as features
+	FROM (
+		SELECT 'Feature' as type,
+		json_build_object('cod_munic', cod_munic,
+		                  'nombre', nombre,
+			          'votos_arena_coalicion', cast((SELECT sum(total_arena_coalision_votos) FROM actas WHERE actas.cod_munic = cod_munic) as integer),
+			          'votos_fmln', CAST((SELECT sum(fmln_votos) FROM actas WHERE actas.cod_munic = cod_munic) as integer),
+			          'votos_gana', CAST((SELECT sum(gana_votos) FORM actas WHERE actas.cod_munic = cod_munic) AS integer),
+			          'votos_vamos', CAST((SELECT sum(vamos_votos) FROM actas WHERE actas.cod_munic = cod_munic) AS integer)) as properties,
+		cast(ST_asGeoJson(pos_geom as json)) as geometry
+		FROM muncipios
+		WHERE cod_dep = {{ cod_dep | guards.integer }}  -- cod_dep is the parameter that we pass here.
+	) AS result_f
+)as result_s1
+{% endsql%}
